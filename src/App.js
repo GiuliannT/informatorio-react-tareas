da6fd7b059721bf9f9691a1f1e82a4dc105fdadf
GiuliannT <giuliconti1@gmail.com>
2022-05-31 20:33:29 -0300

    tarea 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <HomePage />
+    </div>
   );
 };
